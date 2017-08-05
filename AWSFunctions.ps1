196d33ddcc0fc4c85a6c06285741bab8faa487e5
Jas <30461450+jasy125@users.noreply.github.com>
2017-08-05 12:53:40 +0100

    Update AWSFunctions.ps1

--- AWSFunctions.ps1
+++ AWSFunctions.ps1
@@ -1,35 +1,38 @@
 ﻿
-#Create AWS Profile for connections
-
-
-Function CreateAWSProfile($AccessKey,$SecretKey,$ProfileName) {
-
-    Set-AWSCredentials -AccessKey $AccessKey -SecretKey $SecretKey -StoreAs $ProfileName
-
+#What Functions are on this page
+#1. Install Powershell Tools
+#2. Create your AWS Profile Connection file
+#3. Select a AWS Region
+#4. Assign a new Tag to instance
+ 
+#install AWS PowerShell Tools
+Function InstallAWSPowerShell($Path) {
+	 if($Path -eq $null) {
+	     Install-Package -Name AWSPowerShell
+	  } else {
+		Install-Package -Name AWSPowerShell -Path $Path
+	     }
 }
-#CreateAWSProfile("ouraccesskey","oursecureKey","MyProfile"); #Use this to call the function to create your aws profile connection file
-
-
-
+#InstallAWSPowerShell($Path);
+ 
+ 
+ 
+#Create AWS Profile for connections
+Function CreateAWSProfile($AccessKey,$SecretKey,$ProfileName) {
+    Set-AWSCredentials -AccessKey $AccessKey -SecretKey $SecretKey -StoreAs $ProfileName;
+}
+#CreateAWSProfile($AccessKey,$SecretKey,$ProfileName); #Use this to call the function to create your aws profile connection file
 
 
 #Initalise Region
-
-
 Function IntitialzeRegion($ProfileName,$Region) {
-
     Initialize-AWSDefaults -ProfileName $ProfileName -Region $Region
-
 }
-#IntitializeRegion("myprofilename","Region"); #Use this to call the funtion Change Region. 
-
+#IntitializeRegion($ProfileName,$Region); #Use this to call the funtion Change Region. 
 
 
 #Assign a new Tag
-
 Function TagInstance($instanceID,$tag) {
-	
     New-EC2Tag -Resource $instanceID -Tag $tag
-
 }
 #TagInstance($instanceID,$key,$keyVal)
